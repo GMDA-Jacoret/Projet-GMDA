7b703057d58e13cebd6ec346fc08ea66c8ca2284
hippolytej <hippolyte.jacomet@gmail.com>
2017-02-27 18:59:08 +0100

    Minor change

--- analysis.py
+++ analysis.py
@@ -41,7 +41,8 @@ for k, res in enumerate([res_t1, res_t2]):
     hd_df = pd.DataFrame(hd_data, index = hd_index, columns = hd_columns)
 
     # plot
-    hd = hd_df.plot(marker = '.', ax=axes[k],figsize=(10, 4), title = 'Dataset of type %i' %(k+1))
+    hd = hd_df.plot(marker = '.', ax=axes[k],figsize=(10, 4))
+    hd.set_title('Dataset of type %i' %(k+1), fontsize = 10)
     hd.set_xlabel('Doubling dimension')
     hd.set_ylabel('Halving depth')
     hd.legend(['Jit coef : 0', 'Jit coef : 1', 'Jit coef : 3'], loc = 'best', fontsize = 9)
@@ -57,7 +58,8 @@ for k, res in enumerate([res_t1, res_t2]):
     c0_index = res['dd'].unique()
     c0_df = pd.DataFrame(c0_data, index = c0_index)
     # plot
-    c0 = c0_df.plot(marker = '.', ax=axes[k],figsize=(10, 4), title = 'Dataset of type %i' %(k+1))
+    c0 = c0_df.plot(marker = '.', ax=axes[k],figsize=(10, 4))
+    c0.set_title('Dataset of type %i' %(k+1), fontsize = 10)
     c0.set_xlabel('Doubling dimension')
     c0.set_ylabel('c0')
     c0.legend(['Jit coef : 0', 'Jit coef : 1', 'Jit coef : 3'], loc = 'best', fontsize = 9)
@@ -72,7 +74,8 @@ for k, res in enumerate([res_t1, res_t2]):
     c1_index = res['dd'].unique()
     c1_df = pd.DataFrame(c1_data, index = c1_index)
     # plot
-    c1 = c1_df.plot(marker = '.', ax=axes[k],figsize=(10, 4), title = 'Dataset of type %i' %(k+1))
+    c1 = c1_df.plot(marker = '.', ax=axes[k],figsize=(10, 4))
+    c1.set_title('Dataset of type %i' %(k+1), fontsize = 10)
     c1.set_xlabel('Doubling dimension')
     c1.set_ylabel('c1')
     c1.legend(['Jit coef : 0', 'Jit coef : 1', 'Jit coef : 3'], loc = 'best', fontsize = 9)
@@ -89,7 +92,8 @@ for k, res in enumerate([res_t1, res_t2]):
     c2_columns = res['jit_coef'].unique()
     c2_df = pd.DataFrame(c2_data, index = c2_index, columns = c2_columns)
     # plot
-    c2 = c2_df.plot(marker = '.', ax=axes[k],figsize=(10, 4), title = 'Dataset of type %i' %(k+1))
+    c2 = c2_df.plot(marker = '.', ax=axes[k],figsize=(10, 4))
+    c2.set_title('Dataset of type %i' %(k+1), fontsize = 10)
     c2.set_xlabel('Doubling dimension')
     c2.set_ylabel('c2')
     c2.legend(['Jit coef : 0', 'Jit coef : 1', 'Jit coef : 3'], loc = 'best', fontsize = 9)
